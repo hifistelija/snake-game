79b033d0b4a06be8c6e0c1c54d0c6d581b9c6c52
hifistelija <127313571+hifistelija@users.noreply.github.com>
2023-03-26 09:25:02 +0300

    Add files via upload

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+
 ALIGNMENT = "center"
 FONT = ("Courier", 18, "normal")
 
@@ -7,6 +8,7 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = self.read_high_score()
         self.color("white")
         self.penup()
         self.hideturtle()
@@ -15,12 +17,27 @@ class ScoreBoard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.write_high_score()
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
         self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("Game Over", align=ALIGNMENT, font=FONT)
+    def read_high_score(self):
+        try:
+            with open("data.txt", mode="r") as file:
+                self.high_score = int(file.read())
+        except(FileNotFoundError, ValueError):
+            self.high_score = 0
+        return self.high_score
+
+    def write_high_score(self):
+        with open("data.txt", mode="w") as file:
+            file.write(str(self.high_score))
